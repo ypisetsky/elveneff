314efe0726b46339763b281ccd95973504bcdda9
Yuliy Pisetsky <yuliy.pisetsky@gmail.com>
2018-08-20 12:44:32 -0500

    Planks, human images

---
+++ src/humanimg.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import Residence from './humanimg/residence.png';
+import Workshop from './humanimg/workshop.png';
+import Marble from './humanimg/marble.png';
+import Planks from './humanimg/planks.png';
+import Crystal from './humanimg/crystal.png';
+import Steel from './humanimg/steel.png';
+import Elixir from './humanimg/Elixir.png';
+
+module.exports = {
+  Residence,
+  Workshop,
+  Marble,
+  Crystal,
+  Elixir,
+  Steel,
+}
