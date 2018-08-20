c3afd30075309612b2681c07e70e79c0e71ce877
Yuliy Pisetsky <yuliy.pisetsky@gmail.com>
2018-08-20 15:49:11 -0500

    Hook up more images.

--- src/humanimg.js
+++ src/humanimg.js
@@ -1,18 +1,22 @@
 'use strict';
 
-import Residence from './humanimg/residence.png';
-import Workshop from './humanimg/workshop.png';
-import Marble from './humanimg/marble.png';
-import Planks from './humanimg/planks.png';
-import Crystal from './humanimg/crystal.png';
-import Steel from './humanimg/steel.png';
-import Elixir from './humanimg/Elixir.png';
+import Residence from './images/residence.png';
+import Workshop from './images/workshop.png';
+import Marble from './images/marble.png';
+import Steel from './images/steel.png';
+import Planks from './images/planks.png';
+import Crystal from './images/crystal.png';
+import Scrolls from './images/scrolls.png';
+import Elixir from './images/Elixir.png';
+
 
 module.exports = {
   Residence,
   Workshop,
   Marble,
-  Crystal,
-  Elixir,
   Steel,
+  Planks,
+  Crystal,
+  Scrolls,
+  Elixir,
 }
--- src/images.js
+++ src/images.js
@@ -3,15 +3,19 @@
 import Residence from './images/residence.png';
 import Workshop from './images/workshop.png';
 import Marble from './images/marble.png';
-import Crystal from './images/crystal.png';
 import Steel from './images/steel.png';
+import Planks from './images/planks.png';
+import Crystal from './images/crystal.png';
+import Scrolls from './images/scrolls.png';
 import Elixir from './images/Elixir.png';
 
 module.exports = {
   Residence,
   Workshop,
   Marble,
-  Crystal,
-  Elixir,
   Steel,
+  Planks,
+  Crystal,
+  Scrolls,
+  Elixir,
 }
